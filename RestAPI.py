3bd0a3423d7a93fd0e38a243e7b73a6553c25bc7
Senthil <Senthil2312.perf@gmail.com>
2021-02-27 18:35:53 +0530

    Splunk CSV

--- RestAPI.py
+++ RestAPI.py
@@ -1,12 +1,32 @@
 import requests
+import json
+import time
+import csv
 
 data = {
   'id': 'mysearch_02151949',
   'max_count': '50000',
   'status_buckets': '300',
-  'search':'search index=_internal sourcetype=splunkd'
+  'search':'search index=_internal sourcetype=splunkd | stats count by component | sort - count'
 }
 
 response = requests.post('https://localhost:8089/servicesNS/admin/search/search/jobs', data=data, verify=False, auth=('admin', 'Bnymellon123'))
-
 print(response.text)
+
+data = {'output_mode': 'json'}
+time.sleep(10)
+
+splunk_base_url = 'https://localhost:8089/servicesNS/admin/search/search/jobs/mysearch_02151949/results'
+splunk_base_results = requests.get(splunk_base_url, data=data, verify=False, auth=('admin', 'Bnymellon123'))
+splunk_base_data = splunk_base_results.json()
+
+print(splunk_base_data)
+for data in splunk_base_data['results']:
+  print(data)
+
+
+with open ("output.csv", "w") as f:
+  csvfile = csv.writer(f,lineterminator='\n')
+  csvfile.writerow(["component","count"])
+  for item in splunk_base_data['results']:
+    csvfile.writerow([item['component'],item['count']])
