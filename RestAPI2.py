5a15884fecc36f19582254f8c98d344d382c8339
Senthil <Senthil2312.perf@gmail.com>
2021-03-02 07:12:00 +0530

    RestAPI2

--- RestAPI2.py
+++ RestAPI2.py
@@ -7,14 +7,23 @@ data = {
   'id': 'mysearch_02151949',
   'max_count': '50000',
   'status_buckets': '300',
-  'search':'search index=_internal sourcetype=splunkd | stats count by event_message'
+  'search':'| inputlookup one.csv | table event_message'
 }
 
 response = requests.post('https://localhost:8089/servicesNS/admin/search/search/jobs', data=data, verify=False, auth=('admin', 'Bnymellon123'))
 print(response.text)
 
-data = {'output_mode': 'json'}
-time.sleep(10)
+is_job_completed = ''
+
+while (is_job_completed != 'DONE'):
+    time.sleep(5)
+    data = {'output_mode': 'json'}
+    splunk_status_base_url = 'https://localhost:8089/servicesNS/admin/search/search/jobs/mysearch_02151949'
+    resp_job_status = requests.post(splunk_status_base_url, data=data, verify=False, auth=('admin', 'Bnymellon123'))
+    resp_job_status_data = resp_job_status.json()
+    is_job_completed = resp_job_status_data['entry'][0]['content']['dispatchState']
+    print("Current JOB Status is {}".format(is_job_completed))
+
 
 splunk_base_url = 'https://localhost:8089/servicesNS/admin/search/search/jobs/mysearch_02151949/results'
 splunk_base_results = requests.get(splunk_base_url, data=data, verify=False, auth=('admin', 'Bnymellon123'))
@@ -27,6 +36,6 @@ for data in splunk_base_data['results']:
 
 with open ("output.csv", "w") as f:
   csvfile = csv.writer(f,lineterminator='\n')
-  csvfile.writerow(["event_message","count"])
+  csvfile.writerow(["event_message"])
   for item in splunk_base_data['results']:
-    csvfile.writerow([item['event_message'],item['count']])
+    csvfile.writerow([item['event_message']])
