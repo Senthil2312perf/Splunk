be25024e3cf3c672812eee0418456e6c724228cc
Senthil <Senthil2312.perf@gmail.com>
2021-03-02 07:53:29 +0530

    password

--- RestAPI2.py
+++ RestAPI2.py
@@ -2,6 +2,13 @@ import requests
 import json
 import time
 import csv
+import getpass
+
+username = getpass.getpass(prompt="Enter the username...")
+password = getpass.getpass(prompt="Enter the password...")
+
+#print(password)
+
 
 data = {
   'id': 'mysearch_02151949',
@@ -10,7 +17,7 @@ data = {
   'search':'| inputlookup one.csv | table event_message'
 }
 
-response = requests.post('https://localhost:8089/servicesNS/admin/search/search/jobs', data=data, verify=False, auth=('admin', 'Bnymellon123'))
+response = requests.post('https://localhost:8089/servicesNS/admin/search/search/jobs', data=data, verify=False, auth=(username, password))
 print(response.text)
 
 is_job_completed = ''
@@ -19,14 +26,14 @@ while (is_job_completed != 'DONE'):
     time.sleep(5)
     data = {'output_mode': 'json'}
     splunk_status_base_url = 'https://localhost:8089/servicesNS/admin/search/search/jobs/mysearch_02151949'
-    resp_job_status = requests.post(splunk_status_base_url, data=data, verify=False, auth=('admin', 'Bnymellon123'))
+    resp_job_status = requests.post(splunk_status_base_url, data=data, verify=False, auth=(username, password))
     resp_job_status_data = resp_job_status.json()
     is_job_completed = resp_job_status_data['entry'][0]['content']['dispatchState']
     print("Current JOB Status is {}".format(is_job_completed))
 
 
 splunk_base_url = 'https://localhost:8089/servicesNS/admin/search/search/jobs/mysearch_02151949/results'
-splunk_base_results = requests.get(splunk_base_url, data=data, verify=False, auth=('admin', 'Bnymellon123'))
+splunk_base_results = requests.get(splunk_base_url, data=data, verify=False, auth=(username, password))
 splunk_base_data = splunk_base_results.json()
 
 print(splunk_base_data)
